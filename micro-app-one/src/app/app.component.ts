815f0026f45132134faab0db46f3ac024ce7a5a3
Kiran J. Holla <kjholla@gmail.com>
2020-11-20 15:08:34 +0530

    Changing Component prefixes and naming conventions for the various projects

--- micro-app-one/src/app/app.component.ts
+++ micro-app-one/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'micro-app-one',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'micro-app-one';
