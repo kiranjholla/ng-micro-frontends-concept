3643cd18a4781af5d7a867a9e4af3f7eb7c5a8f5
Kiran J. Holla <kjholla@gmail.com>
2020-11-23 20:24:50 +0530

    Adding Bootstrap Logic & Angular Elements logic to Micro App Two

--- micro-app-two/src/app/app.component.ts
+++ micro-app-two/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'micro-app-two',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'micro-app-two';
+
+  constructor(private readonly router: Router) {}
+
+  ngOnInit() {
+    this.router.initialNavigation();
+  }
 }
--- micro-app-two/src/app/app.module.ts
+++ micro-app-two/src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
+import { ApplicationRef, CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyContainer } from './containers/empty/empty.container';
@@ -13,6 +15,21 @@ import { PlantModule } from './modules/plant/plant.module';
   declarations: [AppComponent, EmptyContainer, LandingContainer, HomeContainer],
   imports: [BrowserModule, AppRoutingModule, PlantModule, AnimalModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly injector: Injector) {}
+
+  ngDoBootstrap(appRef: ApplicationRef) {
+    if (!environment.production) {
+      appRef.bootstrap(AppComponent);
+    } else {
+      console.log('Checking 2: ', customElements.get('micro-app-two'));
+      if (!customElements.get('micro-app-two')) {
+        const appWebComponent = createCustomElement(AppComponent, { injector: this.injector });
+        customElements.define('micro-app-two', appWebComponent);
+      }
+    }
+  }
+}
