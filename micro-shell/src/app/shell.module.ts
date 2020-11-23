01a7d0db1bdc11dbf8f2b96460a133a4cfc6be55
Kiran J. Holla <kjholla@gmail.com>
2020-11-23 08:49:19 +0530

    Wiring in the new MicroAppConfigService to the Shell

--- micro-shell/src/app/shell.module.ts
+++ micro-shell/src/app/shell.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { ShellRoutingModule } from './shell-routing.module';
-import { ShellContainer } from './containers/shell/shell.container';
 import { EmptyContainer } from './containers/empty/empty.container';
-import { HomeContainer } from './containers/home/home.container';
 import { ErrorContainer } from './containers/error/error.container';
+import { HomeContainer } from './containers/home/home.container';
 import { PageNotFoundContainer } from './containers/page-not-found/page-not-found.container';
+import { ShellContainer } from './containers/shell/shell.container';
+import { MicroAppConfigModule } from './modules/micro-app-config/micro-app-config.module';
+import { MicroAppConfigService, microAppInitializerFactory } from './modules/micro-app-config/services/micro-app-config.service';
+import { ShellRoutingModule } from './shell-routing.module';
 
 @NgModule({
   declarations: [ShellContainer, HomeContainer, EmptyContainer, ErrorContainer, PageNotFoundContainer],
-  imports: [BrowserModule, ShellRoutingModule],
+  imports: [BrowserModule, ShellRoutingModule, MicroAppConfigModule.configureMicroApps('configs/micro-app-config.json')],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: microAppInitializerFactory,
+      multi: true,
+      deps: [MicroAppConfigService]
+    }
+  ],
   bootstrap: [ShellContainer]
 })
 export class ShellModule {}
